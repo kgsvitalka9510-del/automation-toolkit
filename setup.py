66a7bc34c63975a356c82083c51f696d9ebe59a7
Neptun <kgsvitalka9510@gmail.com>
2026-07-14 09:00:56 +0300

    feat: add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="automation-toolkit",
+    version="1.0.0",
+    packages=find_packages(),
+    author="Agent Developer",
+    description="Python automation toolkit",
+    url="https://github.com/kgsvitalka9510-del/automation-toolkit",
+    python_requires=">=3.8",
+)
